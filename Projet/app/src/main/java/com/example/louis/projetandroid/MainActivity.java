66e2a2c94a440f1d72366d5be0d07c8b77f16f27
Louis Landais <landais.louis@orange.fr>
2019-06-17 17:38:10 +0200

    oui2

--- Projet/app/src/main/java/com/example/louis/projetandroid/MainActivity.java
+++ Projet/app/src/main/java/com/example/louis/projetandroid/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.louis.projetandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Connect(View v)
+    {
+        startActivity(new Intent(this, Vue2.class));
     }
 }
---
+++ Projet/app/src/main/java/com/example/louis/projetandroid/Vue2.java
@@ -0,0 +1,8 @@
+package com.example.louis.projetandroid;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class Vue2 extends AppCompatActivity
+{
+
+}
