c8363183bbd63c0051f2103cf83144bd14973ec5
Louis Landais <landais.louis@orange.fr>
2019-06-17 17:42:59 +0200

    Oui3

--- Projet/app/src/main/java/com/example/louis/projetandroid/MainActivity.java
+++ Projet/app/src/main/java/com/example/louis/projetandroid/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -17,6 +19,20 @@ public class MainActivity extends AppCompatActivity
 
     public void Connect(View v)
     {
-        startActivity(new Intent(this, Vue2.class));
+        EditText edtTxt1 = (EditText) findViewById(R.id.edtTxt1);
+        EditText edtTxt2 = (EditText) findViewById(R.id.edtTxt2);
+
+        String monNom = edtTxt1.getText().toString();
+        String monMdp = edtTxt2.getText().toString();
+
+        if((monNom.equals("Louis")) && (monMdp.equals("abc")))
+        {
+            startActivity(new Intent(this, Vue2.class));
+        }
+        else
+        {
+            Toast.makeText(this, "Erreur de connexion...", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
