4f232c67ee33a132ba17214e92f9cbc4caada200
Louis Landais <landais.louis@orange.fr>
2019-06-17 18:05:56 +0200

    Permissions de localisation

--- Projet/app/src/main/java/com/example/louis/projetandroid/Vue2.java
+++ Projet/app/src/main/java/com/example/louis/projetandroid/Vue2.java
@@ -1,8 +1,22 @@
 package com.example.louis.projetandroid;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class Vue2 extends AppCompatActivity
 {
 
+
+    public void Refresh(View v)
+    {
+        TextView txtv3 = (TextView)findViewById(R.id.txtv3);
+
+        String maPosition = "";
+
+
+
+        txtv3.setText(maPosition);
+
+    }
 }
